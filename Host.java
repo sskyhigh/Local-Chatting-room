76901e8f18b47338f39aead7a500bcaf6ffd47f3
sskyhigh <windowshao_123@yahoo.com>
2022-10-14 17:12:51 -0400

    Update 3

--- Host.java
+++ Host.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.io.IOException;
-
+// run this only after you ran the main file
 public class Host {
     public static void main(String[] args) throws IOException {
         Client client = new Client("127.0.0.1");
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-
+// run this first before running the client.
 public class Main {
     public static void main(String[] args) throws IOException {
         BackEnd server = new BackEnd();
